69dcc3bedab54130d00a6a9f8f0ba799582ce00b
Mohammad Numan <9256fa@gmail.com>
2024-09-22 23:33:51 +0530

    add OOPS 2

--- OOPS Java/OOPS1.java
+++ OOPS Java/OOPS1.java
@@ -52,3 +52,4 @@ public class OOPS1
     }
 
 }
+
--- OOPS Java/OOPS2.java
+++ OOPS Java/OOPS2.java
@@ -1,48 +1,46 @@
 /*
-                        <---------Access Modifiers-------->
+ * Types of Access Modifiers in Java
+ * There are four types of access modifiers available in Java:
+ * 
+ * 1. Default – No keyword required
+ * 2. Private
+ * 3. Protected
+ * 4. Public
+ * 
+ * 1: When no access modifier is specified for a class, method, or data member – It is said to be having the default access modifier by default---> accessible only within the same package.
+ * 
+ * 2:  The methods or data members declared as private are accessible only within the class in which they are declared.Any other class of the same package will not be able to access these members.
+ * 
+ * 
+ * 3: The methods or data members declared as protected are accessible within the same package or subclasses in different packages.
+ */
 
-There are four types of access modifiers available in Java: 
-
-Default – No keyword required
-1. Private
-2. Protected
-3. Public
-
-1: When no access modifier is specified for a class, method, or data member – It is said to be having the default access modifier by default.--->Accessible only within the same package
-
-2: The methods or data members declared as private are accessible only within the class in which they are declared---->Any other class of the same package will not be able to access these members.
-
-3: The methods or data members declared as protected are accessible within the same package or subclasses in different packages.
-
-4: Classes, methods, or data members that are declared as public are accessible from everywhere in the program. There is no restriction on the scope of public data members.
-*/
-
-class BankAccount {
+class BankAcc {
     public String username;
     private String password;
 
-    void setpassword(String pwd) {
+    // function for assigning value to password
+    void setPass(String pwd) {
         password = pwd;
     }
-    void display(){
-        System.out.println(username);
-        System.out.println(password);
+
+    // function for displaying value of the variables
+    void display() {
+        System.out.println("User: " + username);
+        System.out.println("Pass: " + password);
     }
+
 }
 
 public class OOPS2 {
     public static void main(String args[]) {
-        BankAccount myAcc = new BankAccount(); // Creating object of class as myAcc
+        BankAcc myAcc = new BankAcc(); // Creating object of BacnkAcc class as myAcc
         myAcc.username = "Numan";
-        /*
-         * myAcc.password="abcdefji"
-         * CAN NOT ACCESS THE PASSWORD BCZ OF PRIVATE MODIFIER-->not visible,cannot be
-         * accessed outside the class
-         */
+        // myAcc.password="abcdefghi"; //Can not be Accessed due to pvt Access
+        // modifier---->Not visible
 
-        myAcc.setpassword("abcdefgi"); // pass set through function which is in the class
-        
-        //Will display the username and password
-        myAcc.display();  
+        myAcc.setPass("abcdefghi"); // Value assigned indirectly to pvt->password using Method of the same class
+        myAcc.display();
     }
+
 }
